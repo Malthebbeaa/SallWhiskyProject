b38b8e9c60c72f6078a9440becc4998178f47439
Malthebb <malthebb@gmail.com>
2024-11-22 14:13:17 +0100

    Tilføjet Distillering og Kommentar

--- src/application/model/Distillering.java
+++ src/application/model/Distillering.java
@@ -1,4 +1,31 @@
 package application.model;
 
+import java.time.LocalDate;
+
 public class Distillering {
+    private int antalDistilleringer;
+    private LocalDate startDato, slutDato;
+    private double væskeMængde;
+    private double alkoholProcent;
+    private Maltbatch maltBatch;
+    private Kommentar kommentar;
+
+    public Distillering(int antalDistilleringer, LocalDate startDato, LocalDate slutDato,
+                        double væskeMængde, double alkoholProcent, Maltbatch maltBatch) {
+        this.antalDistilleringer = antalDistilleringer;
+        this.startDato = startDato;
+        this.slutDato = slutDato;
+        this.væskeMængde = væskeMængde;
+        this.alkoholProcent = alkoholProcent;
+        this.maltBatch = maltBatch;
+    }
+
+    /***
+     * tilføjer en evt kommentar til distilleringen
+     * @param kommentar evt kommentar til distillering
+     */
+    public void setKommentar(Kommentar kommentar){
+        this.kommentar = kommentar;
+    }
+
 }
---
+++ src/application/model/Kommentar.java
@@ -0,0 +1,17 @@
+package application.model;
+
+public class Kommentar {
+    private String kommentar;
+
+    public Kommentar(String kommentar) {
+        this.kommentar = kommentar;
+    }
+
+    public void setKommentar(String kommentar) {
+        this.kommentar = kommentar;
+    }
+
+    public String getKommentar() {
+        return kommentar;
+    }
+}
