e4b5c01993232a81f6271f72abde04816d9e4017
andersna <andersna@stofanet.dk>
2024-11-23 12:48:56 +0100

    Opdateret Maltbatch vindue

--- src/gui/MaltbatchWindow.java
+++ src/gui/MaltbatchWindow.java
@@ -5,13 +5,14 @@ import application.model.Maltbatch;
 import application.model.Mark;
 import com.sun.media.jfxmedia.events.NewFrameEvent;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Paint;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -36,23 +37,73 @@ public class MaltbatchWindow {
         maltPane.setVgap(10);
         maltPane.setPadding(new Insets(20));
 
+        HBox HbMarkNavn = new HBox(10);
+        Label lblMarkNavn = new Label("Marknavn: ");
+        TextField txfMarkNavn = new TextField();
+        HbMarkNavn.getChildren().addAll(lblMarkNavn,txfMarkNavn);
+
+        HBox HbØkologisk = new HBox(10);
+        Label lblØkologisk = new Label("Økologisk");
+        CheckBox cbØkologisk = new CheckBox();
+        HbØkologisk.getChildren().addAll(lblØkologisk,cbØkologisk);
+
+        VBox vBoxMark = new VBox(20);
+        vBoxMark.setPadding(new Insets(20,50,50,10));
+        vBoxMark.getChildren().addAll(HbMarkNavn,HbØkologisk);
+        vBoxMark.setBorder(Border.stroke(Paint.valueOf("Black")));
+
+        TitledPane tPaneMark = new TitledPane("Mark",vBoxMark);
+
+        maltPane.add(tPaneMark,0,0,2,1);
+
+        HBox HbSort = new HBox(10);
+        Label lblSort = new Label("Sort: ");
+        TextField txfSort = new TextField();
+        HbSort.getChildren().addAll(lblSort,txfSort);
+
+        HBox HbHøstdag = new HBox(10);
+        Label lblHøstdag = new Label("Høstdag");
+        DatePicker dpHøstdag = new DatePicker();
+        dpHøstdag.setValue(LocalDate.now());
+        HbHøstdag.getChildren().addAll(lblHøstdag,dpHøstdag);
+
+        VBox vBoxKorn = new VBox(20);
+        vBoxKorn.setPadding(new Insets(20,50,50,10));
+        vBoxKorn.getChildren().addAll(HbSort,HbHøstdag);
+        vBoxKorn.setBorder(Border.stroke(Paint.valueOf("Black")));
+
+        TitledPane tPaneKorn = new TitledPane("Korn",vBoxKorn);
+
+        maltPane.add(tPaneKorn,2,0,2,1);
+
         //Disse ting er kun lavet for at kunne teste.
+
+        VBox vBoxKornVælger = new VBox(10);
+        Label lblKornSortVælger = new Label("Vælg Korn:");
+
         Mark nybogård = new Mark("Nybogård", true);
         cbKorn = new ComboBox<>();
         cbKorn.getItems().add(new Korn(LocalDate.of(2024, 11, 22), "Evergreen", nybogård));
-        maltPane.add(cbKorn, 0, 0,2,1);
+
+        vBoxKornVælger.getChildren().addAll(lblKornSortVælger,cbKorn);
 
         Label lblBatchNummer = new Label("Batchnummer: ");
-        maltPane.add(lblBatchNummer,0,1);
-
         TextField txfBatchnummer = new TextField();
-        maltPane.add(txfBatchnummer,1,1);
+        HBox hBoxBatchNummer = new HBox(10);
+        hBoxBatchNummer.getChildren().addAll(lblBatchNummer,txfBatchnummer);
 
         Label lblMængde = new Label("Mængde: ");
-        maltPane.add(lblMængde,0,2);
-
         TextField txfMængde = new TextField();
-        maltPane.add(txfMængde,1,2);
+
+        HBox hBoxMængde = new HBox(10);
+        hBoxMængde.getChildren().addAll(lblMængde,txfMængde);
+
+        VBox vBoxMaltbatch = new VBox(20);
+        vBoxMaltbatch.setPadding(new Insets(20,50,50,10));
+        vBoxMaltbatch.setBorder(Border.stroke(Paint.valueOf("Black")));
+        vBoxMaltbatch.getChildren().addAll(vBoxKornVælger,hBoxBatchNummer,hBoxMængde);
+
+        maltPane.add(vBoxMaltbatch,0,1,2,1);
 
         Button btnOpretMaltbatch = new Button("Opret Maltbatch");
         btnOpretMaltbatch.setOnAction(e-> opretMaltbatchAktion());
@@ -60,8 +111,11 @@ public class MaltbatchWindow {
         Button btnAfbrydAktion = new Button("Afbryd");
         btnAfbrydAktion.setOnAction(e-> afbrydAktion());
 
-        maltPane.add(btnOpretMaltbatch,0,3);
-        maltPane.add(btnAfbrydAktion,1,3);
+        HBox hBoxButtons = new HBox(10);
+        hBoxButtons.getChildren().addAll(btnOpretMaltbatch,btnAfbrydAktion);
+        hBoxButtons.setAlignment(Pos.CENTER);
+
+        maltPane.add(hBoxButtons,1,4);
     }
 
     public void opretMaltbatchAktion(){
