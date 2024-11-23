d55e817f6933e30e910117ac84b49e7b6709159f
Malthebb <malthebb@gmail.com>
2024-11-23 13:32:04 +0100

    tilføjede destillering til storage og implementerede interface i Storageklassen

--- src/storage/Storage.java
+++ src/storage/Storage.java
@@ -1,14 +1,34 @@
 package storage;
 
+import application.model.Destillering;
 import application.model.Maltbatch;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class Storage {
+public class Storage implements StorageInterface{
 
     private ArrayList<Maltbatch> maltbatches = new ArrayList<>();
+    private ArrayList<Destillering> destilleringer = new ArrayList<>();
+
 
     public void addMaltbatch(Maltbatch maltbatch){
         maltbatches.add(maltbatch);
+    }
+
+    @Override
+    public void addDestillering(Destillering destillering) {
+        destilleringer.add(destillering);
+    }
+
+
+    //----------------------------------------------------------------------------
+    //overvej om vi vil returnere den samme liste eller en kopi?
+    public List<Maltbatch> getMaltbatches(){
+        return maltbatches;
+    }
+    @Override
+    public List<Destillering> getDestilleringer() {
+        return destilleringer;
     }
 }
--- src/storage/StorageInterface.java
+++ src/storage/StorageInterface.java
@@ -1,8 +1,15 @@
 package storage;
 
+import application.model.Destillering;
 import application.model.Maltbatch;
+
+import java.util.List;
 
 public interface StorageInterface {
 
     public void addMaltbatch(Maltbatch maltbatch);
+    public void addDestillering(Destillering destillering);
+
+    public List<Maltbatch> getMaltbatches();
+    public List<Destillering> getDestilleringer();
 }
