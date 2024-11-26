249ca74d859fd09734b697d4251e5aef6a38b88d
Andre <andrelh@live.dk>
2024-11-26 13:16:47 +0100

    Historie og WhiskyProdukt ændret

--- src/application/model/Historie.java
+++ src/application/model/Historie.java
@@ -1,28 +1,31 @@
 package application.model;
 
 public class Historie {
-    public String historie;
 
 
-    public Historie(String historie) {
-        this.historie = historie;
-    }
-
-// "Whiskyen er lavet på" + Kornnavn + " fra vores medejer Lars økologiske mark(er)" + Marknavn(e)
-// + ". Den er destilleret " + AntalDestilleringer + " af maltbatch " + Maltbatch
-// + ". Derefter er den lagret på "  + AntalFade + " lavet af " + Fadmateriale(r)
-// + ", der tidligere er blevet brugt til " + TidligereFyld + ". Der har vi lagret den i "
-// + ÅrLagret + " hvorefter den er blevet hældt på flaske " + PåfyldningsDato "."
-//
-// "Der er lavet " + AntalFlasker + " af denne whiskyBatch
 
 
     public void skrivHistorie(WhiskyProdukt produkt) {
-//        System.out.println("Whiskyen er lavet af " + produkt.getKorn()
-//                + " fra vores medejer Lars mark " + produkt.getMark() + ". ");
-//        if (produkt.getFade().size() > 1) {
-//            System.out.println("Den er lagret på " + produkt.getFade().size() + " fade"
-//                    + "");
+        System.out.println("Whiskyen er lavet af " + produkt.getKorn()
+                + " fra vores medejer Lars mark " + produkt.getMark() + ". ");
+        if (produkt.getFade().size() > 1) {
+            System.out.println("Den er lagret på " + produkt.getFade().size() + " fade ");
+        } else {
+            System.out.println("Den er lagret på fad fra " + produkt.getLand());
 //        }
+//        if (produkt.getFade().indexOf(0).getTidligereIndhold() != produkt.getFade().indexOf(1).getTidligereIndhold()) {
+//            System.out.println(", der tidligere har været brugt til at lagre ");
+//            for (int i = produkt.getFade().size() + 1; i == produkt.getFade().size() - 1; i++) {
+//                System.out.println(produkt.getFade().getFirst().getTidligereIndhold() + " og "
+//                        + produkt.getFade().getLast().getTidligereIndhold());
+//            }
+//        } else {
+//            System.out.println(produkt.getFade().indexOf(0).getTidligereIndhold());
+//        }
+            System.out.println(". Derefter er det blevet lagret i " + produkt.getAarLagret()
+                    + " og hældt på flaske den " + produkt.getDatoTappet());
+            System.out.println("\n. Der er blevet tappet " + produkt.getAntalFlasker());
+
+        }
     }
 }
