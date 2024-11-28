f3ec9a53e54f5023e03a7fe6f3a783667fac1bb2
Malthebb <malthebb@gmail.com>
2024-11-28 21:27:17 +0100

    Listview hvor man kan vælge flere destilleringer

--- src/gui/påfyldning/PåfyldningForm.java
+++ src/gui/påfyldning/PåfyldningForm.java
@@ -4,11 +4,9 @@ import application.controller.Controller;
 import application.model.Destillering;
 import application.model.Fad;
 import gui.PaneCreator;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 
 import java.time.LocalDate;
 
@@ -16,6 +14,7 @@ public class PåfyldningForm {
     private Controller controller;
     private ComboBox<Destillering> cboxDestillering;
     private ComboBox<Fad> cboxFad;
+    //private ListView<Destillering> lvwMuligeDestilleringer, lveValgtDestilleringer;
     private DatePicker datePickerPåfyldningsDato;
     private TextField txfMængdeTilPåfyldning;
     private GridPane påfyldningsPane, påfyldningsInfoPane;
@@ -34,17 +33,44 @@ public class PåfyldningForm {
         påfyldningsPane.setHgap(10);
         påfyldningsPane.setVgap(10);
 
-        Label lblDestillering = new Label("Vælg destillering:");
+        Label lblDestillering = new Label("Vælg destillering(er):");
         påfyldningsInfoPane.add(lblDestillering, 0,0);
-        cboxDestillering = new ComboBox<>();
-        cboxDestillering.setItems(controller.getStorage().getDestilleringer());
-        påfyldningsInfoPane.add(cboxDestillering, 1,0);
+        ListView<Destillering> lvwMuligeDestilleringer = new ListView<>();
+        ListView<Destillering> lveValgtDestilleringer = new ListView<>();
+        lvwMuligeDestilleringer.getItems().addAll(controller.getStorage().getDestilleringer());
+        lveValgtDestilleringer.setPrefSize(100,100);
+        lvwMuligeDestilleringer.setPrefSize(100,100);
+
+
+        Button btnAddSelected = new Button(">");
+        btnAddSelected.setOnAction(e -> {
+            Destillering selected = lvwMuligeDestilleringer.getSelectionModel().getSelectedItem();
+            if (selected != null){
+                lvwMuligeDestilleringer.getItems().remove(selected);
+                lveValgtDestilleringer.getItems().add(selected);
+            }
+        });
+        Button btnRemoveSelected = new Button("<");
+        btnRemoveSelected.setOnAction(e -> {
+            Destillering selected = lveValgtDestilleringer.getSelectionModel().getSelectedItem();
+            if (selected != null){
+                lvwMuligeDestilleringer.getItems().add(selected);
+                lveValgtDestilleringer.getItems().remove(selected);
+            }
+        });
+        VBox buttonBox = new VBox(1, btnAddSelected, btnRemoveSelected);
+
+        påfyldningsInfoPane.add(lvwMuligeDestilleringer, 0,1);
+        påfyldningsInfoPane.add(lveValgtDestilleringer, 2,1);
+        påfyldningsInfoPane.add(buttonBox, 1, 1);
+
+        /*
 
         Label lblFad = new Label("Vælg Fad:");
-        påfyldningsInfoPane.add(lblFad, 2,0);
+        påfyldningsInfoPane.add(lblFad, 3,0);
         cboxFad = new ComboBox<>();
         cboxFad.setItems(controller.getStorage().getFade());
-        påfyldningsInfoPane.add(cboxFad,3,0);
+        påfyldningsInfoPane.add(cboxFad,4,0);
 
 
         Label lblPåfyldningsDato = new Label("Påfyldningsdato: ");
@@ -59,6 +85,8 @@ public class PåfyldningForm {
         txfMængdeTilPåfyldning.setMaxWidth(75);
         påfyldningsInfoPane.add(txfMængdeTilPåfyldning, 3,1);
 
+
+         */
 
     }
 
