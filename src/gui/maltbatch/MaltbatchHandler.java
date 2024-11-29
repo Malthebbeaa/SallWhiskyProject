51e2b896f0c04f3a3faac052190b3bc8a972c6ca
andersna <andersna@stofanet.dk>
2024-11-29 21:15:13 +0100

    Errorhandling

--- src/gui/maltbatch/MaltbatchHandler.java
+++ src/gui/maltbatch/MaltbatchHandler.java
@@ -3,6 +3,7 @@ package gui.maltbatch;
 import application.controller.Controller;
 import application.model.Korn;
 import application.model.Mark;
+import javafx.scene.control.Alert;
 
 import java.time.LocalDate;
 
@@ -15,28 +16,55 @@ public class MaltbatchHandler {
 
     public void opretMaltbatchHandler(MaltbatchForm form){
         String batchnummer = form.getBatchnummer();
-        double mængde = form.getMængde();
+        String mængde = form.getMængde();
         Korn korn = form.getKorn();
-
-        controller.opretMaltbatch(batchnummer, mængde, korn);
-        form.clearAktion();
+        if(batchnummer.isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.WARNING, "Du skal angive et batchnummer inden du fortsætter");
+            alert.showAndWait();
+        }
+        else if(mængde.isEmpty() || !mængde.matches("\\d+")){
+            Alert alert = new Alert(Alert.AlertType.WARNING, "Du skal angive en gyldig mængde inden du fortsætter");
+            alert.showAndWait();
+        }
+        else if(korn == null){
+            Alert alert = new Alert(Alert.AlertType.WARNING, "Du skal vælge en kornsort inden du fortsætter");
+            alert.showAndWait();
+        }
+        else {
+            double mængdeDouble = Double.parseDouble(mængde);
+            controller.opretMaltbatch(batchnummer, mængdeDouble, korn);
+            form.clearAktion();
+        }
     }
 
     public void opretKornHandler(MaltbatchForm form){
         String sort = form.getSort();
         LocalDate høstDag = form.getHøstDag();
         Mark mark = form.getMark();
-
-        controller.opretKorn(høstDag,sort,mark);
-        form.clearAktion();
-
+        if(sort.isEmpty()){
+            Alert alert = new Alert(Alert.AlertType.WARNING, "Du skal angive en sort inden du fortsætter");
+            alert.showAndWait();
+        }
+        else if(mark == null){
+            Alert alert = new Alert(Alert.AlertType.WARNING, "Du skal vælge en mark inden du fortsætter");
+            alert.showAndWait();
+        }
+        else {
+            controller.opretKorn(høstDag, sort, mark);
+            form.clearAktion();
+        }
     }
 
     public void opretMarkHandler(MaltbatchForm form){
         String markNavn = form.getMarkNavn();
         boolean økologisk = form.getIsØkologisk();
-
-        controller.opretMark(markNavn, økologisk);
+        if (markNavn.isEmpty()){
+            Alert alert = new Alert(Alert.AlertType.WARNING, "Du skal vælge et navn til marken inden du fortsætter");
+            alert.showAndWait();
+        }
+        else {
+            controller.opretMark(markNavn, økologisk);
+        }
         form.clearAktion();
     }
 }
